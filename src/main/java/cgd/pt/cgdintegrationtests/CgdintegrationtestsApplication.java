19c8b35b945db5730fc6cc2a01dd07137911bab1
Gustavo Gois <gustavo.gois@gmail.com>
2020-01-20 09:51:32 +0000

    Initial commit

---
+++ src/main/java/cgd/pt/cgdintegrationtests/CgdintegrationtestsApplication.java
@@ -0,0 +1,13 @@
+package cgd.pt.cgdintegrationtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgdintegrationtestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgdintegrationtestsApplication.class, args);
+    }
+
+}
